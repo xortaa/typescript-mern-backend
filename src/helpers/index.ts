86a3508bbe7d217a74c017c7aac2d2fe4b943573
xortaa <kercwinpaul@gmail.com>
2024-01-03 17:40:11 +0800

    authentication helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,7 @@
+import crypto from 'crypto'
+import dotenv from 'dotenv'
+
+export const random = () => crypto.randomBytes(128).toString('base64')
+export const authentication = (salt: string, password: string) => { 
+  return crypto.createHmac('sha26', [salt, password].join('/')).update(process.env.SECRET).digest('hex')
+}
